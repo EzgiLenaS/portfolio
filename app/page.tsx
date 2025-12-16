b15b6766efc383edb4fcebd73979af05618c1dbd
EzgiLenaS <ezgilena00@gmail.com>
2025-12-16 04:35:12 +0300

    dependencies are upgraded

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <main className="h-full w-full">
       <div className="flex flex-col gap-1">
         <Hero />
-        <Skills />
         <Projects />
+        <Skills />
       </div>
     </main>
   );
